fba7183a309ecf0aa0217f669bf9cbd40307d35e
JamesB <jblazer1@yahoo.com>
2019-02-14 09:43:00 -0800

    Add functionality to modal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 //=========================== Navigation Modal ============================================
+const openMenu = document.querySelector(".open");
+const closeMenu = document.querySelector(".close");
+
+// Open
+const openNav = () => {
+  document.getElementById("myNav").style.height = "100%";
+  openMenu.classList.toggle("close-menu");
+  closeMenu.classList.toggle("close-menu");
+};
+
+// Close
+const closeNav = () => {
+  document.getElementById("myNav").style.height = "0%";
+  openMenu.classList.toggle("close-menu");
+  closeMenu.classList.toggle("close-menu");
+};
